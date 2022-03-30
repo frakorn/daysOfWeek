1b1118ff0560f0e9af43b57f6a30e9e22dfaa8f0
Francesco Cini <francesco.cini@gruppometa.it>
2022-03-30 11:39:59 +0200

    first commit

---
+++ src/app/days-of-week/days-of-week.component.spec.ts
@@ -0,0 +1,25 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { DaysOfWeekComponent } from './days-of-week.component';
+
+describe('DaysOfWeekComponent', () => {
+  let component: DaysOfWeekComponent;
+  let fixture: ComponentFixture<DaysOfWeekComponent>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      declarations: [ DaysOfWeekComponent ]
+    })
+    .compileComponents();
+  });
+
+  beforeEach(() => {
+    fixture = TestBed.createComponent(DaysOfWeekComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
